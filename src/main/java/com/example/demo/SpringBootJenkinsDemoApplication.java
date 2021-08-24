51fedc5d25b89b9399328ba642b5f8bf1ae44cf7
example <example@gmail.com>
2021-08-24 15:24:02 +0530

    Second Commit

--- src/main/java/com/example/demo/SpringBootJenkinsDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootJenkinsDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootJenkinsDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome");
 		SpringApplication.run(SpringBootJenkinsDemoApplication.class, args);
 	}
 
